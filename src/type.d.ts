9cc3ae6b2b4ab1a3f89720bb13167b16b60c6a32
heppokofrontend <heppokofrontend@gmail.com>
2021-07-22 23:30:10 +0900

    :wrench: Add type file

---
+++ src/type.d.ts
@@ -0,0 +1,11 @@
+interface JQueryStatic {
+  a11yName(): string;
+}
+
+type FormAssociated = HTMLInputElement |
+                      HTMLButtonElement |
+                      HTMLSelectElement |
+                      HTMLTextAreaElement |
+                      HTMLOutputElement |
+                      HTMLProgressElement |
+                      HTMLMeterElement;
