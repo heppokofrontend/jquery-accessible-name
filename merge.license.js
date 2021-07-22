e9e7767f1851f6d133f7dda119381fbf109e174a
heppokofrontend <heppokofrontend@gmail.com>
2021-07-23 04:13:51 +0900

    :zap: ライセンステキストの一体化

--- merge.license.js
+++ merge.license.js
@@ -1,16 +1,24 @@
+const path = require('path');
 const fs = require('fs-extra');
-const licensePath = './lib/jquery.accessible-name.min.js.LICENSE.txt';
-const licenseText = fs.readFileSync(licensePath,  {
-  encoding: 'utf-8',
-}).trim();
-const bannerText = '/*! For license information please see jquery.accessible-name.min.js.LICENSE.txt */';
-const jsCode = fs.readFileSync('./lib/jquery.accessible-name.min.js', {
-  encoding: 'utf-8',
-});
+const filelist = fs.readdirSync('./lib').filter(p => p.endsWith('.js'));
+const entries = filelist.map(p => [
+  path.join('lib', p),
+  path.join('lib', `${p}.LICENSE.txt`),
+]);
 
-fs.writeFileSync(
-  './lib/jquery.accessible-name.min.js',
-  jsCode.replace(bannerText, licenseText)
-);
+for (const [filePath, licensePath] of entries) {
+  const licenseText = fs.readFileSync(licensePath,  {
+    encoding: 'utf-8',
+  }).trim();
+  const bannerText = /\/\*! For license information please see (.*?)\*\//;
+  const jsCode = fs.readFileSync(filePath, {
+    encoding: 'utf-8',
+  });
 
-fs.removeSync(licensePath);
+  fs.writeFileSync(
+    filePath,
+    jsCode.replace(bannerText, licenseText)
+  );
+
+  fs.removeSync(licensePath);
+}
