5fac8d211c5aeebe43dfab32520c24a056f7ec0a
heppokofrontend <heppokofrontend@gmail.com>
2021-07-23 04:18:17 +0900

    :zap: Update

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,11 +1,16 @@
+// ? ラベルを提供している要素を取得するモードとかあってもいいかも
+// interface JQueryA11yNameOptions {
+
+// }
+
 interface JQuery {
   a11yName(): string;
 }
 
-type FormAssociated = HTMLInputElement |
-                      HTMLButtonElement |
-                      HTMLSelectElement |
-                      HTMLTextAreaElement |
-                      HTMLOutputElement |
-                      HTMLProgressElement |
-                      HTMLMeterElement;
+interface Window {
+  jQuery: JQueryStatic
+}
+
+type FormAssociated = HTMLInputElement | HTMLButtonElement | HTMLSelectElement |
+                      HTMLTextAreaElement | HTMLOutputElement |
+                      HTMLProgressElement | HTMLMeterElement;
